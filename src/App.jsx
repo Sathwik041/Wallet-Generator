7f6fe44f4baf563b0bb4e3c6dffe7b218cfaecb0
Sathwik Annaldesh <93048377+Sathwik041@users.noreply.github.com>
2025-03-12 13:04:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from './SolanaWallet';
+import { EthWallet } from './EthWallet';
+
+function App() {
+  const [mnemonic, setMnemonic] = useState("");
+
+  return (
+    <>
+      <input 
+        type="text" 
+        value={mnemonic} 
+        onChange={(e) => setMnemonic(e.target.value)} 
+        placeholder="Enter or generate seed phrase"
+      />
+      <button onClick={async function() {
+        const mn = generateMnemonic();
+        setMnemonic(mn)
+      }}>
+        Create Seed Phrase
+      </button>
+
+      {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
+      {mnemonic && <EthWallet mnemonic={mnemonic} />}
+    </>
+  )
+}
+
+export default App
